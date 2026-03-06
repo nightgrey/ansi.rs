d9404dfb64ad954c8388fcf0e8fde59b8ae8b2a5
Nico <nightgrey@users.noreply.github.com>
2026-03-06 20:05:12 +0100

    wip

--- crates/grid/src/features/mod.rs
+++ crates/grid/src/features/mod.rs
@@ -1,13 +1,13 @@
 mod step;
 mod into_location;
 mod span;
-mod iterator;
+mod steps;
 mod located;
 mod into_slice_index;
 
 pub use into_location::*;
 pub use span::*;
-pub use iterator::*;
+pub use steps::*;
 pub use located::*;
 pub use into_slice_index::*;
 pub use step::*;

44e30045aacf9c2d1e207d7265fd70c60a5a6b36
Nico <nightgrey@users.noreply.github.com>
2026-03-12 17:02:03 +0100

    wip

--- crates/sigil/src/bin/sandbox.rs
+++ crates/sigil/src/bin/sandbox.rs
@@ -5,20 +5,15 @@ fn main() -> io::Result<()> {
     tree()?;
     Ok(())
 }
-
-
 pub fn tree() -> io::Result<()> {
     let mut engine = Engine::new(30, 5);
 
     let header = engine
-        .elements
-        .insert(Element::text("=== Header ===".to_string()));
+        .append_root(Element::text("=== Header ===".to_string()));
     let body = engine
-        .elements
-        .insert(Element::text("Hello, world!".to_string()));
+        .append_root(Element::text("Hello, world!".to_string()));
     let footer = engine
-        .elements
-        .insert(Element::text("=== Footer ===".to_string()));
+        .append_root(Element::text("=== Footer ===".to_string()));
 
     // Render
     let mut stdout = io::stdout();

36639a8a0f36afa58551befd1d5dc232a402c696
Nico <nightgrey@users.noreply.github.com>
2026-05-28 23:21:56 +0200

    wip

--- crates/gloss/src/raster/mod.rs
+++ crates/gloss/src/raster/mod.rs
@@ -2,10 +2,8 @@ mod pen;
 mod rasterer;
 mod counting;
 mod presenter;
-mod presenter_stats;
 
 pub use pen::*;
 pub use rasterer::*;
 pub use counting::*;
 pub use presenter::*;
-pub use presenter_stats::*;
